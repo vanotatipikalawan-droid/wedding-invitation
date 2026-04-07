f04eff4b9c7871446aa189365969d24c842e9db7
vanotatipikalawan-droid <vanotatipikalawan@gmail.com>
2026-04-08 00:40:35 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "Wedding Invitation" };
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div style={{padding:40}}>Wedding App Ready 🚀<br/>Buka /invite/vanno-ima</div>;
+}
