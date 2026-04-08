8d8aebd42808ae78ad1ab73f999591fbd964e004
vanotatipikalawan-droid <vanotatipikalawan@gmail.com>
2026-04-09 05:38:42 +0700

    Update page.tsx

--- app/invite/[slug]/page.tsx
+++ app/invite/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../../lib/supabase";
 
 export default async function Page({ params }: any) {
   const { data } = await supabase
