b8b827ef3b4267737d0393e57a2deb4255af4e26
vanotatipikalawan-droid <vanotatipikalawan@gmail.com>
2026-04-09 06:09:20 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Wedding Invitation" };
-
-export default function RootLayout({ children }: any) {
-  return (
-    <html lang="id">
-      <body>{children}</body>
-    </html>
-  );
-}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div style={{padding:40}}>Wedding App Ready 🚀<br/>Buka /invite/vanno-ima</div>;
-}
